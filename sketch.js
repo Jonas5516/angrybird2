121773300a280ed09521dee172228e4159be635d
Jonas5516 <nastyjet55@gmail.com>
2020-12-01 17:27:59 -0800

    added pig

--- sketch.js
+++ sketch.js
@@ -3,7 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1, pig1;
+var box1, pig1, pig5;
 var backgroundImg,platform;
 
 function preload() {
@@ -34,7 +34,8 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
-    bird = new Bird(100,100);
+    bird = new Bird(100,100);   
+    pig5 = new Pig(100,200)
 
 }
 
@@ -60,5 +61,6 @@ function draw(){
     log5.display();
 
     bird.display();
+    pig5.display();
     platform.display();
 }
